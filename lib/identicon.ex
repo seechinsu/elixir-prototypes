1daf197d9a90c803c2d8b573109cc75e3a3b09c4
seechinsu <seechinsu@gmail.com>
2018-08-24 10:22:25 -0400

    added additional methods and struct

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -18,12 +18,53 @@ defmodule Identicon do
 
   def main(input) do
     input
-    |>hash_input
+    |> hash_input
+    |> pick_color
+    |> build_grid
+    |> filter_odd_squares
+    # |> build_pixel_map
+  end
+
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+
+  end
+
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+    grid =
+      Enum.filter grid, fn {value, _index} ->
+      rem(value, 2) == 0
+    end
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    grid =
+      hex
+      |> Enum.chunk_every(3,3, :discard)
+      |> mirror_row
+      |> List.flatten
+      |> Enum.with_index
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def mirror_row(hex) do
+    hex
+    |>Enum.map(fn x ->
+      Enum.concat(x, Enum.reverse(Enum.take(x, 2)))
+    end)
+  end
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
   end
 
   def hash_input(input) do
-    :crypto.hash(:md5, input)
+    hex = :crypto.hash(:md5, input)
     |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
   end
 
 end
