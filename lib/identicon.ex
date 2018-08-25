c3a1274a161a8125b61d681dfb05a469fc93095d
seechinsu <seechinsu@gmail.com>
2018-08-25 01:27:07 -0400

    added draw image and save image functions

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -22,11 +22,35 @@ defmodule Identicon do
     |> pick_color
     |> build_grid
     |> filter_odd_squares
-    # |> build_pixel_map
+    |> build_pixel_map
+    |> draw_image
+    |> save_image(input)
+  end
+
+  def save_image(image, input) do
+    File.write("#{input}.png", image)
+  end
+
+  def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
+    image = :egd.create(250, 250)
+    fill = :egd.color(color)
+
+    Enum.each pixel_map, fn({start,stop}) ->
+        :egd.filledRectangle(image, start, stop, fill)
+    end
+
+    :egd.render(image)
   end
 
   def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    pixel_map = Enum.map grid, fn({_value, index}) ->
+      {
+        {rem(index,5) * 50, div(index,5) * 50} ,
+        {(rem(index,5) * 50) + 50, (div(index,5) * 50) + 50}
+      }
+    end
 
+    %Identicon.Image{image | pixel_map: pixel_map}
   end
 
   def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
